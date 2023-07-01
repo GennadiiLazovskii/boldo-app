e505e8af179975c16a93ac13ffb1da5292dc39f0
Gena Lazovskiy <genalazovskiy@MacBook-Pro-Gena.local>
2023-07-01 19:04:11 +0300

    Add: MainScrean

--- src/components/mainScrean/mainScrean.jsx
+++ src/components/mainScrean/mainScrean.jsx
@@ -1,5 +1,8 @@
 import styles from './mainScrean.module.scss';
 import BoldoLogo from '../../images/main/logoBoldo.png';
+import BoldoLogoWhite from '../../images/main/logoBoldoWhite.png';
+import Graphics from '../../images/main/graphicsgraf.png';
+import PrestoLogo from '../../images/main/presto-logo.png';
 
 const MainScrean = () => {
   return (
@@ -16,11 +19,31 @@ const MainScrean = () => {
           </div>
         </div>
         <div className={styles.mainCenter}>
-          
+          <div className={styles.mainCenterLeft}>
+            <div className={styles.mainCenterLeftTitle}>
+              <h1>Save time by building fast with Boldo Template</h1>
+            </div>
+            <div className={styles.mainCenterLeftBtn}>
+              <button className={styles.mainCenterBtnGreen}>Buy template</button>
+              <button className={styles.mainCenterBtnWhite}>Explore</button>
+            </div>
+          </div>
+          <div className={styles.mainCenterRight}>
+            <img src={Graphics} alt="Graphics" />
+          </div>
+        </div>
+        <div className={styles.mainBottom}>
+          <div className={styles.scrollingPhotos}>
+            <img src={BoldoLogoWhite} alt="BoldoLogo" />
+            <img src={PrestoLogo} alt="PrestoLogo" />
+            <img src={BoldoLogoWhite} alt="BoldoLogo" />
+            <img src={PrestoLogo} alt="PrestoLogo" />
+          </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default MainScrean;
+

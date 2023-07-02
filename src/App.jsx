9b59eac13f488180801139069057e8e018d2eeb5
Gena Lazovskiy <genalazovskiy@MacBook-Pro-Gena.local>
2023-07-02 14:30:43 +0300

    Add: infographicScrean

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import InfographicScrean from './components/infographicScrean/infographicScrean'
 import MainScrean from './components/mainScrean/mainScrean'
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
   return (
     <>
       <MainScrean/>
+      <InfographicScrean/>
     </>
   )
 }
---
+++ src/components/infographicScrean/infographicScrean.jsx
@@ -0,0 +1,39 @@
+import styles from './infographicScrean.module.scss';
+import MenGreen from '../../images/infographic/gren-m.png';
+import Men from '../../images/infographic/men.png';
+import Houses from '../../images/infographic/houses.png';
+
+const InfographicScrean = () => {
+
+  return (
+    <div className={styles.infographicScrean}>
+      <div className={styles.container}>
+        <div className={styles.infographicMassMarket}>
+          <h2 className={styles.infographicMassMarketTitle}>Handshake infographic mass market crowdfunding iteration.</h2>
+          <div className={styles.infographicMassMarketBlock}>
+            <div className={styles.infographicMassMarketBlockCard}>
+              <img src={MenGreen} alt="Men" />
+              <p className={styles.CardTitle}>Cool feature title</p>
+              <p className={styles.CardDescr}>Learning curve network effects return on investment.</p>
+              <p className={styles.CardBtn}>Explore page</p>
+            </div>
+            <div className={styles.infographicMassMarketBlockCard}>
+              <img src={Men} alt="Men" />
+              <p className={styles.CardTitle}>Cool feature title</p>
+              <p className={styles.CardDescr}>Learning curve network effects return on investment.</p>
+              <p className={styles.CardBtn}>Explore page</p>
+            </div>
+            <div className={styles.infographicMassMarketBlockCard}>
+              <img src={Houses} alt="Houses" />
+              <p className={styles.CardTitle}>Cool feature title</p>
+              <p className={styles.CardDescr}>Learning curve network effects return on investment.</p>
+              <p className={styles.CardBtn}>Explore page</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default InfographicScrean;
